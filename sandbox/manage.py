06d81251d4180fd9b6c0264d2c658e7dcbdc3631
your-username <georgiosgiannopoulos1993@gmail.com>
2024-11-09 13:37:33 +0200

    Updated python path and manage.py

--- sandbox/manage.py
+++ sandbox/manage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    load_dotenv(os.path.join(project_dir, '.env'))
+    src_path = os.path.join(project_dir, '..', 'src')
+    if src_path not in sys.path:
+        sys.path.insert(0, src_path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
